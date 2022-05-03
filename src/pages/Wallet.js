0499cb0994e6d627cb0b332949d7f43a4306aa92
marllomartin <marllon3636@gmail.com>
2022-05-03 15:59:53 -0300

    style(pages/Wallet.js): Ajusta página de carteira para CSS

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Form from '../components/Form';
 import Header from '../components/Header';
 import Table from '../components/Table';
+import '../styles/Wallet/styles.css';
 
 class Wallet extends React.Component {
   render() {
     return (
 
-      <div>
-        <Header />
-        <Form />
+      <div className="wallet-page">
+        <div className="wallet-card">
+          <Header />
+          <Form />
+        </div>
         <Table />
       </div>
     );
