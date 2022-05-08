3f2244a986e9142ce802ddd082a5cc3da41467e3
marllomartin <marllon3636@gmail.com>
2022-05-08 18:51:55 -0300

    feat(src/.): Cria página 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 import Wallet from './pages/Wallet';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/carteira" component={ Wallet } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     </div>
   );
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import notFound from '../animations/notfound-animate.svg';
+import '../styles/NotFound/styles.css';
+
+export default class NotFound extends React.Component {
+  render() {
+    return (
+      <div className="notfound-page">
+        <img src={ notFound } alt="Página não encontrada."/>
+      </div>
+    )
+  }
+}
